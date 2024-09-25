86a161e12f3a31e5d63d862cc2acfd4f81b83ced
Analice <analice.leite12@gmail.com>
2024-09-25 14:43:17 -0300

    🐛 Bugfix no device available text after disconecting de sound captation

--- script.py
+++ script.py
@@ -87,4 +87,4 @@ def get_selected_device():
     return jsonify(device=selected_device)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug= False)
+    app.run(host='0.0.0.0', port=5000, debug= True)
